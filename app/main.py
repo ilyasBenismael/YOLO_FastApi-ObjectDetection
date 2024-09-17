b548528640858d5904fc0ad82bc200884154acac
ilyasBenismael <73490018+ilyasBenismael@users.noreply.github.com>
2024-09-17 21:28:48 +0100

    444

--- app/main.py
+++ app/main.py
@@ -4,13 +4,11 @@ from ultralytics import YOLO
 from fastapi import FastAPI, WebSocket
 import websockets
 import time
+from fastapi.responses import HTMLResponse
 
 
 
 
-
-
-myapp = FastAPI()
 
 # model = torch.hub.load('ultralytics/yolov5', 'yolov5n', device='cpu')
 
@@ -55,13 +53,55 @@ myapp = FastAPI()
 
 
 
+
+
+myapp = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@myapp.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
 @myapp.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    # Handle a WebSocket connection
-    i = 5
+    await websocket.accept()
     while True:
-        # data = await websocket.receive_text()
-        time.sleep(5)
-        await websocket.send_text(f"The value of i is {i+1}")
-        
+        data = await websocket.receive_text()
+        await websocket.send_text("abooooood")
+
 
