9b91d939ef24d1dcf993088ef2ae09ec3c0e8254
ilyasBenismael <73490018+ilyasBenismael@users.noreply.github.com>
2024-09-17 21:38:41 +0100

    3

--- app/main.py
+++ app/main.py
@@ -72,7 +72,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("ws://safedrivefastapi-production.up.railway.app/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
