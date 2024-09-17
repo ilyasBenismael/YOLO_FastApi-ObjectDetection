65f49f45faba76e39aa66e77d0bbca26e7b85813
ilyasBenismael <73490018+ilyasBenismael@users.noreply.github.com>
2024-09-17 15:09:47 +0100

    44

--- app/main.py
+++ app/main.py
@@ -12,53 +12,56 @@ import time
 
 myapp = FastAPI()
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5n', device='cpu')
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5n', device='cpu')
 
-img = Image.open('pics/lgdg.jpg')
+# img = Image.open('pics/lgdg.jpg')
 
-results = model(img)
+# results = model(img)
 
-results.save('results')
+# results.save('results')
 
-classes_of_interest = [0, 1, 3, 2, 5, 7, 9, 11, 15, 16, 17, 18, 19]
+# classes_of_interest = [0, 1, 3, 2, 5, 7, 9, 11, 15, 16, 17, 18, 19]
 
-detected_objects = []
+# detected_objects = []
 
 
 # Access details of detected objects like bounding boxes, confidence, and labels
-for pred in results.pred:
-    for obj in pred:
-        bbox = obj[:4]  # Bounding box coordinates (x1, y1, x2, y2)
-        confidence = obj[4]  # Confidence score
-        class_id = int(obj[5])  # Class ID
+# for pred in results.pred:
+#     for obj in pred:
+#         bbox = obj[:4]  # Bounding box coordinates (x1, y1, x2, y2)
+#         confidence = obj[4]  # Confidence score
+#         class_id = int(obj[5])  # Class ID
 
-        # Filter only the classes of interest
-        if class_id in classes_of_interest:
-            class_name = results.names[class_id]  # Get the class name            
-            # Create a map (dictionary) for the detected object
-            detected_obj = {
-                'class_name': class_name,
-                'bbox': bbox.tolist(),  # Convert tensor to list
-                'confidence': round(confidence.item(), 2),  # Convert to float and round off
-                'class_id': class_id
-            }
+#         # Filter only the classes of interest
+#         if class_id in classes_of_interest:
+#             class_name = results.names[class_id]  # Get the class name            
+#             # Create a map (dictionary) for the detected object
+#             detected_obj = {
+#                 'class_name': class_name,
+#                 'bbox': bbox.tolist(),  # Convert tensor to list
+#                 'confidence': round(confidence.item(), 2),  # Convert to float and round off
+#                 'class_id': class_id
+#             }
             
-            # Add the detected object map to the list
-            detected_objects.append(detected_obj)
+#             # Add the detected object map to the list
+#             detected_objects.append(detected_obj)
 
 
 
-@myapp.get("/")
-def returnResult():
-    return 'heey brother'
+# @myapp.get("/")
+# def returnResult():
+#     return 'heey brother'
+
+
 
 
 @myapp.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     # Handle a WebSocket connection
+    i = 5
     while True:
         # data = await websocket.receive_text()
-        time.sleep(10)
-        await websocket.send_text("heey")
+        time.sleep(5)
+        await websocket.send_text(f"The value of i is {i+1}")
         
 
