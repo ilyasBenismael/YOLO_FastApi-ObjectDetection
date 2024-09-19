639eb64748f655c6f05e75e6984674b6ab1ab7b3
ilyasBenismael <73490018+ilyasBenismael@users.noreply.github.com>
2024-09-19 14:37:34 +0100

    4447

--- app/main.py
+++ app/main.py
@@ -100,25 +100,24 @@ async def websocket_endpoint(websocket: WebSocket):
             img_buffer = BytesIO()
             Image.fromarray(results.ims[0]).save(img_buffer, format='JPEG')
             rendered_image_bytes = img_buffer.getvalue()
-            print(rendered_image_bytes)
-            # await websocket.send_bytes(rendered_image_bytes)
+            await websocket.send_bytes(rendered_image_bytes)
            
 
             # Filter results by classes of interest
-            detected_objects = []
-            detected_objects_names = []
-            for pred in results.pred[0]:  # Loop over detections
-                class_id = int(pred[-1])  # The class ID is the last value
-                if class_id in classes_of_interest:
-                    detected_objects.append({
-                        'class': model.names[class_id],
-                        'confidence': float(pred[4]),  # Confidence score
-                        'bbox': [float(pred[0]), float(pred[1]), float(pred[2]), float(pred[3])]  # Bounding box
-                    })
-                    detected_objects_names.append(model.names[class_id])
+            # detected_objects = []
+            # detected_objects_names = []
+            # for pred in results.pred[0]:  # Loop over detections
+            #     class_id = int(pred[-1])  # The class ID is the last value
+            #     if class_id in classes_of_interest:
+            #         detected_objects.append({
+            #             'class': model.names[class_id],
+            #             'confidence': float(pred[4]),  # Confidence score
+            #             'bbox': [float(pred[0]), float(pred[1]), float(pred[2]), float(pred[3])]  # Bounding box
+            #         })
+            #         detected_objects_names.append(model.names[class_id])
 
-            # Send the detection results back to the client as JSON
-            await websocket.send_json({'detections': detected_objects_names})
+            # # Send the detection results back to the client as JSON
+            # await websocket.send_json({'detections': detected_objects_names})
 
     except Exception as e:
         await websocket.send_text(e)
